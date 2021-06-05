ed6f4f1c3d5f896841ceb83f8de7c91b381f5a4d
sumana2001 <sumana.basu2001@gmail.com>
2021-06-06 04:14:37 +0530

    nav

--- src/components/NavDashboard.js
+++ src/components/NavDashboard.js
@@ -1,8 +1,8 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 import { Link } from "react-router-dom";
-import React, { useState, useEffect } from 'react';
-import { FaBars, FaTimes, FaMagento} from 'react-icons/fa';
-import { IconContext } from 'react-icons/lib';
+import React, { useState, useEffect } from "react";
+import { FaBars, FaTimes, FaMagento } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
 
 const Nav = styled.nav`
   background: #000;
@@ -13,7 +13,7 @@ const Nav = styled.nav`
   font-size: 1.2rem;
   position: sticky;
   top: 0;
-  z-index:10000;
+  z-index: 10000;
 `;
 
 const NavbarContainer = styled.div`
@@ -43,27 +43,12 @@ const NavLogo = styled(Link)`
   display: flex;
   align-items: center;
   &:hover {
-    color:#fff;
-    text-decoration:none;
+    color: #fff;
+    text-decoration: none;
     transition: all 0.3s ease;
   }
-`;
-
-const NavIcon = styled(FaMagento)`
-  margin-right: 0.5rem;
-`;
-
-const MobileIcon = styled.div`
-  display: none;
-
-  @media screen and (max-width: 960px) {
-    display: block;
-    position: absolute;
-    top: 0;
-    right: 0;
-    transform: translate(-100%, 60%);
-    font-size: 1.8rem;
-    cursor: pointer;
+  @media screen and (max-width: 768px) {
+    font-size: 20px;
   }
 `;
 
@@ -72,107 +57,20 @@ const NavMenu = styled.ul`
   align-items: center;
   list-style: none;
   text-align: center;
-
-  @media screen and (max-width: 960px) {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    height: 90vh;
-    position: absolute;
-    top: 80px;
-    left: ${({ click }) => (click ? 0 : '-100%')};
-    opacity: 1;
-    transition: all 0.5s ease;
-    background: #000;
-
-
-  }
-`;
-
-const NavItem = styled.li`
-
-  @media screen and (max-width: 960px) {
-    width: 100%;
-
-  }
-`;
-
-
-const NavLinks = styled(Link)`
-margin-top:15px;
-  color: #fff;
-  display: flex;
-  align-items: center;
-  text-decoration: none;
-  padding: 0.5rem 1rem;
-  height: 100%;
-  cursor:pointer;
-  &:hover {
-    color:#fff;
-    text-decoration:none;
-    transition: all 0.3s ease;
-    border-bottom: 2px solid royalblue;
-  }
-
-  @media screen and (max-width: 960px) {
-    text-align: center;
-    padding: 2rem;
-    width: 100%;
-    display: table;
-
-    &:hover {
-      color:royalblue;
-      border:none;
-      transition: all 0.3s ease;
-    }
-  }
 `;
 
 function Navbar() {
-  const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
-
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
-
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
-
-  useEffect(() => {
-    showButton();
-  }, []);
-
-  window.addEventListener('resize', showButton);
-
   return (
     <>
-      <IconContext.Provider value={{ color: '#fff' }}>
+      <IconContext.Provider value={{ color: "#fff" }}>
         <Nav>
           <NavbarContainer>
-            <NavLogo
-            to="/" onClick={closeMobileMenu}>
-              THE REGINA
-            </NavLogo>
-            <MobileIcon onClick={handleClick}>
-              {click ? <FaTimes /> : <FaBars />}
-            </MobileIcon>
-            
-            <NavMenu onClick={handleClick} click={click}>
-            <NavItem>
-            <NavLinks to="/update-profile" onClick={closeMobileMenu}>
-            Update Profile
-          </NavLinks>
-          </NavItem>
-          <NavItem>
-            <NavLinks to="/update-profile" onClick={closeMobileMenu}>
-            Logout
-          </NavLinks>
-          </NavItem>
+            <NavLogo to="/">THE REGINA</NavLogo>
+
+            <NavMenu>
+              <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
+                Update Profile
+              </Link>
             </NavMenu>
           </NavbarContainer>
         </Nav>
